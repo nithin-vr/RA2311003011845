8332dfed47f558d18d0ffb1dbbd6f2b979a032cb
Nithin VR <vrnithin123@example.com>
2026-05-02 11:10:08 +0530

    add frontend config with credentials

---
+++ notification_app_fe/config.js
@@ -0,0 +1,9 @@
+export const CONFIG = {
+  BASE_URL: "http://20.207.122.201/evaluation-service",
+  CLIENT_ID: "28b1722c-4eff-4d66-a424-eb469c91024e",
+  CLIENT_SECRET: "nVyrxWaDzmjJBQGy",
+  ACCESS_CODE: "QkbpxH",
+  EMAIL: "vn9503@srmist.edu.in",
+  NAME: "vr nithin",
+  ROLL_NO: "ra2311003011845"
+};
